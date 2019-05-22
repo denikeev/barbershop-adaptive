2dfab777a90f994477f3abd486b10976baf30119
denikeev <script.edf@gmail.com>
2019-05-22 16:48:27 +0300

    Команда gulp sass - работает без ошибок

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require("gulp");
   var sass = require("gulp-sass");
   gulp.task("sass", function() {
-    gulp.src("source/sass/style.scss")
+    return gulp.src("source/sass/style.scss")
       .pipe(sass())
       .pipe(gulp.dest("source/css"));
   })
