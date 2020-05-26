ade6d729fc4b88dd8ba1cfc53c82e188cbc7cba2
denikeev <script.edf@gmail.com>
2020-05-26 09:40:36 +0300

    добавлена сборка build

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,10 @@ var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+
+var del = require("del");
 
 
 gulp.task("css", function () {
@@ -23,16 +27,16 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream())
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("source/css"));
+    .pipe(gulp.dest("build/css"));
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -40,7 +44,8 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  // gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html",  gulp.series("html"));
 });
 
 gulp.task("images", function () {
@@ -65,9 +70,53 @@ gulp.task("sprite", function () {
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
 
 gulp.task("start", gulp.series("css", "server"));
 
+// 
+ gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+
+gulp.task("build", 
+  gulp.series(
+    'clean', 
+    'copy', 'css',
+    'sprite',
+    'html'
+));
+
+// gulp.task("build", function (done) {
+//   run(
+//     "clean",
+//     "copy",
+//     "css",
+//     "sprite",
+//     "html",
+//     done
+//   );
+// });
+
+// gulp.task('build', ["clean", "copy", "css", "sprite", "html"]);
